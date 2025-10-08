46f3d84faaafd519c89d941658611b636b11e070
AdachiRei0 <jzfs157@163.com>
2025-10-08 22:28:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // 当页面加载完成后执行
 document.addEventListener('DOMContentLoaded', function() {
     // 获取h1标签
-    const heading = document.querySelector('h1');
+    const h1 = document.querySelector('h1');
     // 为它添加点击事件
-    heading.addEventListener('click', function() {
+    h1.addEventListener('click', function() {
         alert('你点击了标题！');
     });
 });
